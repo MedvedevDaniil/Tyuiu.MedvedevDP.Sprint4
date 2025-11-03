c3b60edfc3537ae0fdea8152431eaebb96a3dacf
danii <danii@DANIIL>
2025-11-03 17:21:32 +0500

    12

--- Tyuiu.MedvedevDP.Sprint4.Task2.V7.Lib/DataService.cs
+++ Tyuiu.MedvedevDP.Sprint4.Task2.V7.Lib/DataService.cs
@@ -9,7 +9,7 @@ namespace Tyuiu.MedvedevDP.Sprint4.Task2.V7.Lib
             int sum = 0;
             for (int i = 0; i < array.Length; i++)
             {
-                if (array[i] % 2 == 0)
+                if (array[i] % 2 == 0 )
                 {
                     sum += array[i];
                 }
