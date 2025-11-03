5a738ea2e0fea2169881b0f695bf465a8bb300e4
danii <danii@DANIIL>
2025-11-03 17:18:32 +0500

    task2

---
+++ Tyuiu.MedvedevDP.Sprint4.Task2.V7.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Tyuiu.MedvedevDP.Sprint4.Task2.V7.Lib;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tyuiu.MedvedevDP.Sprint4.Task2.V7.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalculate()
+        {
+            DataService ds = new DataService();
+
+            int[] numsArray = { 4, 5, 6, 7, 8, 9, 4, 6, 8, 5, 7, 9 };
+            int res = ds.Calculate(numsArray);
+
+            int wait = 36;
+
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.MedvedevDP.Sprint4.Task2.V7/Program.cs
@@ -0,0 +1,36 @@
+﻿using Tyuiu.MedvedevDP.Sprint4.Task2.V7.Lib;
+using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Random rnd = new Random();
+        DataService ds = new DataService();
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        int len = 12; 
+
+        int[] numsArray = new int[len];
+
+        Console.Write("Массив (заполнен случайными числами от 4 до 9): ");
+        for (int i = 0; i <= len - 1; i++)
+        {
+            numsArray[i] = rnd.Next(4, 10);
+            Console.Write(numsArray[i] + "\t");
+        }
+        Console.WriteLine();
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        int res = ds.Calculate(numsArray);
+        Console.WriteLine("Сумма чётных элементов массива: " + res);
+
+        Console.ReadKey();
+    }
+}
