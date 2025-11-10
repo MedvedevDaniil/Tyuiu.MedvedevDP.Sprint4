d6f78424bee95f5355a56e9ad45e943f478ee776
danii <danii@DANIIL>
2025-11-10 16:32:09 +0500

    task5

---
+++ Tyuiu.MedvedevDP.Sprint4.Task5.V23.Test/DataServiceTest.cs
@@ -0,0 +1,29 @@
+﻿using Tyuiu.MedvedevDP.Sprint4.Task5.V23.Lib;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tyuiu.MedvedevDP.Sprint4.Task5.V23.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+
+            int[,] matrix = new int[,] { { -1, 2, -3 }, { 4, -5, 6 }, { -7, 8, 0 } };
+
+            int[,] expected = new int[,] { { 0, 2, 0 }, { 4, 0, 6 }, { 0, 8, 0 } };
+
+            int[,] actual = ds.Calculate(matrix);
+
+            for (int i = 0; i < expected.GetLength(0); i++)
+            {
+                for (int j = 0; j < expected.GetLength(1); j++)
+                {
+                    Assert.AreEqual(expected[i, j], actual[i, j]);
+                }
+            }
+        }
+    }
+}
---
+++ Tyuiu.MedvedevDP.Sprint4.Task5.V23/Program.cs
@@ -0,0 +1,46 @@
+﻿using Tyuiu.MedvedevDP.Sprint4.Task5.V23.Lib;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        DataService ds = new DataService();
+        Random rnd = new Random();
+
+        int rows = 5;
+        int cols = 5;
+        int[,] matrix = new int[rows, cols];
+
+        Console.WriteLine("ИСХОДНЫЙ МАССИВ:");
+        Console.WriteLine("***************************************************************************");
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                matrix[i, j] = rnd.Next(-4, 9); 
+                Console.Write($"{matrix[i, j],3} ");
+            }
+            Console.WriteLine();
+        }
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("РЕЗУЛЬТАТ:");
+        Console.WriteLine("***************************************************************************");
+
+        int[,] resultMatrix = ds.Calculate(matrix);
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                Console.Write($"{resultMatrix[i, j],3} ");
+            }
+            Console.WriteLine();
+        }
+
+        Console.WriteLine("***************************************************************************");
+        Console.ReadKey();
+    }
+}
